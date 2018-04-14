2213bf2da6ac4495d28b0f85924cbb6ffc04bd07
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-04-14 21:01:15 +0100

    Move cahrt area component into separate file

---
+++ src/ChartArea.jsx
@@ -0,0 +1,88 @@
+import * as d3 from 'd3';
+import React from 'react';
+
+class ChartArea extends React.Component {
+    render() {
+        const svg = d3.select('svg');
+
+        svg.selectAll('*').remove();
+
+        const margin = { top: 10, right: 10, bottom: 30, left: 30 };
+        const width = this.props.width - margin.left - margin.left;
+        const height = this.props.height - margin.top - margin.bottom;
+
+        const g = svg.append('g')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+
+        const xScale = d3.scaleTime()
+            .range([0, width])
+            .domain(d3.extent(this.props.data, function (d) { return d3.isoParse(d.from); }));
+
+        const maxIntensity = d3.max(
+            this.props.data,
+            function (d) { return d3.max([d.intensity.actual, d.intensity.forecast]); })
+
+        const yScale = d3.scaleLinear()
+            .range([height, 0])
+            .domain([0, 1.05 * maxIntensity]);
+
+        const xAxis = g.append('g')
+            .call(d3.axisBottom(xScale))
+            .attr('transform', 'translate(0,' + height + ')')
+
+        const yAxis = g.append('g')
+            .call(d3.axisLeft(yScale))
+
+        const actualIntensityLine = g.append('path')
+            .datum(this.props.data)
+            .attr('d', d3.line()
+                .x(function (d) { return xScale(d3.isoParse(d.from)); })
+                .y(function (d) { return yScale(d.intensity.actual); }))
+            .attr('fill', 'none')
+            .attr('stroke', 'black');
+
+        const forecastIntensityLine = g.append('path')
+            .datum(this.props.data)
+            .attr('d', d3.line()
+                .x(function (d) { return xScale(d3.isoParse(d.from)); })
+                .y(function (d) { return yScale(d.intensity.forecast); }))
+            .attr('fill', 'none')
+            .attr('stroke', 'red')
+            .attr('stroke-dasharray', '2, 2');
+
+        const tooltipGroup = g.append('g')
+            .attr('display', 'none');
+
+        const tooltipLine = tooltipGroup.append('line')
+            .attr('y1', 0)
+            .attr('y2', height)
+            .attr('stroke', 'black');
+
+        const tooltipText = tooltipGroup.append('text');
+
+        const chartArea = g.append('rect')
+            .attr('width', width)
+            .attr('height', height)
+            .attr('fill', 'none')
+            .attr('pointer-events', 'all')
+            .on('mouseover', function () { tooltipGroup.attr('display', null); })
+            .on('mouseout', function () { tooltipGroup.attr('display', 'none'); })
+            .on('mousemove', function () {
+                const xPixel = d3.mouse(this)[0];
+                const xValue = xScale.invert(xPixel);
+                tooltipLine.attr('transform', 'translate(' + xPixel + ',0)');
+                tooltipText
+                    .attr('transform', 'translate(' + xPixel + ',' + height + ')')
+                    .attr('dx', '0.50em')
+                    .attr('dy', '-0.50em')
+                    .text(xValue);
+            });
+
+        return <svg width={this.props.width} height={this.props.height}
+            visibility={this.props.visible ? null : 'hidden'} />
+    }
+}
+
+export default ChartArea;
