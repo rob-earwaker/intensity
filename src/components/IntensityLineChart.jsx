41fbd8f6623839e48d41d0a458b5f6f4b27cbe65
Rob Earwaker <rob-earwaker@users.noreply.github.com>
2018-04-18 22:10:48 +0100

    Convert from class to function

--- src/components/IntensityLineChart.jsx
+++ src/components/IntensityLineChart.jsx
@@ -15,111 +15,113 @@ const ForecastIntensityLine = styled(Line) `
     stroke-dasharray: 2, 2;
 `;
 
-class IntensityLineChart extends React.Component {
-    indexColour(index) {
-        return {
-            'very low': 'blue',
-            low: 'green',
-            moderate: 'yellow',
-            high: 'orange',
-            'very high': 'red'
-        }[index];
-    }
+function mapIntensityIndexToColour(index) {
+    return {
+        'very low': 'blue',
+        low: 'green',
+        moderate: 'yellow',
+        high: 'orange',
+        'very high': 'red'
+    }[index];
+}
 
-    render() {
-        const svg = d3.select('svg');
+function IntensityLineChart(props) {
+    const svg = d3.select('svg');
 
-        const margin = { top: 10, right: 10, bottom: 30, left: 30 };
-        const width = this.props.width - margin.left - margin.left;
-        const height = this.props.height - margin.top - margin.bottom;
+    const margin = { top: 10, right: 10, bottom: 30, left: 30 };
+    const width = props.width - margin.left - margin.left;
+    const height = props.height - margin.top - margin.bottom;
 
-        const g = svg.append('g')
-            .attr('width', width)
-            .attr('height', height)
-            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+    const g = svg.append('g')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-        const xScale = d3.scaleTime()
-            .range([0, width])
-            .domain(d3.extent(this.props.data, d => d3.isoParse(d.from)));
+    const timeAccessor = d => d3.isoParse(d.from);
+    const actualIntensityAccessor = d => d.intensity.actual;
+    const forecastIntensityAccessor = d => d.intensity.forecast;
 
-        const maxIntensity = d3.max(
-            this.props.data,
-            d => d3.max([d.intensity.actual, d.intensity.forecast]))
+    const xScale = d3.scaleTime()
+        .range([0, width])
+        .domain(d3.extent(props.data, timeAccessor));
 
-        const yScale = d3.scaleLinear()
-            .range([height, 0])
-            .domain([0, 1.05 * maxIntensity]);
+    const maxIntensity = d3.max(
+        props.data,
+        d => d3.max([actualIntensityAccessor(d), forecastIntensityAccessor(d)]))
 
-        const xAxis = g.append('g')
-            .call(d3.axisBottom(xScale))
-            .attr('transform', 'translate(0,' + height + ')')
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, 1.05 * maxIntensity]);
 
-        const yAxis = g.append('g')
-            .call(d3.axisLeft(yScale))
+    const xAxis = g.append('g')
+        .call(d3.axisBottom(xScale))
+        .attr('transform', 'translate(0,' + height + ')')
 
-        const tooltipGroup = g.append('g')
-            .attr('display', 'none');
+    const yAxis = g.append('g')
+        .call(d3.axisLeft(yScale))
 
-        const tooltipLine = tooltipGroup.append('line')
-            .attr('y1', 0)
-            .attr('y2', height)
-            .attr('stroke', 'black');
+    const tooltipGroup = g.append('g')
+        .attr('display', 'none');
 
-        const tooltipText = tooltipGroup.append('text');
+    const tooltipLine = tooltipGroup.append('line')
+        .attr('y1', 0)
+        .attr('y2', height)
+        .attr('stroke', 'black');
 
-        const chartArea = g.append('rect')
-            .attr('width', width)
-            .attr('height', height)
-            .attr('fill', 'none')
-            .attr('pointer-events', 'all')
-            .on('mouseover', () => tooltipGroup.attr('display', null))
-            .on('mouseout', () => tooltipGroup.attr('display', 'none'))
-            .on('mousemove', function () {
-                const xPixel = d3.mouse(this)[0];
-                const xValue = xScale.invert(xPixel);
-                tooltipLine.attr('transform', 'translate(' + xPixel + ',0)');
-                tooltipText
-                    .attr('transform', 'translate(' + xPixel + ',' + height + ')')
-                    .attr('dx', '0.50em')
-                    .attr('dy', '-0.50em')
-                    .text(xValue);
-            });
+    const tooltipText = tooltipGroup.append('text');
 
-        return (
-            <svg
-                viewBox={'0 0 ' + this.props.width + ' ' + this.props.height}
-                visibility={this.props.visible ? null : 'hidden'}>
-                <g
-                    width={width}
-                    height={height}
-                    transform={'translate(' + margin.left + ',' + margin.top + ')'}>
-                    <ActualIntensityLine
-                        data={this.props.data}
-                        xScale={xScale}
-                        xAccessor={d => d3.isoParse(d.from)}
-                        yScale={yScale}
-                        yAccessor={d => d.intensity.actual}
-                    />
-                    <ForecastIntensityLine
-                        data={this.props.data}
-                        xScale={xScale}
-                        xAccessor={d => d3.isoParse(d.from)}
-                        yScale={yScale}
-                        yAccessor={d => d.intensity.forecast}
-                    />
-                    <CircleMarkers
-                        data={this.props.data}
-                        xScale={xScale}
-                        xAccessor={d => d3.isoParse(d.from)}
-                        yScale={yScale}
-                        yAccessor={d => d.intensity.actual}
-                        r={2}
-                        fillAccessor={d => this.indexColour(d.intensity.index)}
-                    />
-                </g>
-            </svg>
-        )
-    }
+    const chartArea = g.append('rect')
+        .attr('width', width)
+        .attr('height', height)
+        .attr('fill', 'none')
+        .attr('pointer-events', 'all')
+        .on('mouseover', () => tooltipGroup.attr('display', null))
+        .on('mouseout', () => tooltipGroup.attr('display', 'none'))
+        .on('mousemove', function () {
+            const xPixel = d3.mouse(this)[0];
+            const xValue = xScale.invert(xPixel);
+            tooltipLine.attr('transform', 'translate(' + xPixel + ',0)');
+            tooltipText
+                .attr('transform', 'translate(' + xPixel + ',' + height + ')')
+                .attr('dx', '0.50em')
+                .attr('dy', '-0.50em')
+                .text(xValue);
+        });
+
+    return (
+        <svg
+            viewBox={'0 0 ' + props.width + ' ' + props.height}
+            visibility={props.visible ? null : 'hidden'}>
+            <g
+                width={width}
+                height={height}
+                transform={'translate(' + margin.left + ',' + margin.top + ')'}>
+                <ActualIntensityLine
+                    data={props.data}
+                    xScale={xScale}
+                    xAccessor={timeAccessor}
+                    yScale={yScale}
+                    yAccessor={actualIntensityAccessor}
+                />
+                <ForecastIntensityLine
+                    data={props.data}
+                    xScale={xScale}
+                    xAccessor={timeAccessor}
+                    yScale={yScale}
+                    yAccessor={forecastIntensityAccessor}
+                />
+                <CircleMarkers
+                    data={props.data}
+                    xScale={xScale}
+                    xAccessor={timeAccessor}
+                    yScale={yScale}
+                    yAccessor={actualIntensityAccessor}
+                    r={2}
+                    fillAccessor={d => mapIntensityIndexToColour(d.intensity.index)}
+                />
+            </g>
+        </svg>
+    )
 }
 
 export default IntensityLineChart;
